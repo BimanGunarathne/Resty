e1ce1b7b7f88eb687405ff04d31cb14ace605990
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-02-09 22:22:18 +0530

    ``` Refactor App component for improved readability and maintainability ```

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,19 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Searchbar } from 'react-native-paper';
 
 interface HomeScreenProps{
     navigation?:any;
 }
 
 const HomeScreen: React.FC<HomeScreenProps> = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View>
-          <Text>Home Screen</Text>
+        <View className="p-5">
+          <Searchbar className='bg-[#f5f5f5]' placeholder="Search for menus, restaurants and hotels" onChangeText={setSearchQuery} value={searchQuery} icon="magnify"/>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
